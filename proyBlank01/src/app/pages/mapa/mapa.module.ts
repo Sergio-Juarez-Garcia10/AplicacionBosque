d7b16007736d03f1edf6cfe37a8c3d1979fe0c1a
Sergio-Juarez-Garcia10 <54821824+Sergio-Juarez-Garcia10@users.noreply.github.com>
2019-10-22 00:33:09 -0500

    sergio
    xcvb

--- proyBlank01/src/app/pages/mapa/mapa.module.ts
+++ proyBlank01/src/app/pages/mapa/mapa.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
-
+// import { GoogleMaps } from '@ionic-native/google-maps';
 import { IonicModule } from '@ionic/angular';
 
 import { MapaPage } from './mapa.page';
